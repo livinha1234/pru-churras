cda95d7534070336c8a52ea1ec89bf2439cb4980
livinha1234 <liviamariasouza268@gmail.com>
2023-03-16 12:29:39 -0300

    Arrumar Programação

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,95 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+numero = 1;
+pont1 = 0;
+pont2 = 0;
+vitoria1 = 0;
+vitoria2 = 0;
+
+
   constructor() {}
 
+
+  clicarBotao1() {
+
+    this.numero = 1;
+
+  }
+
+  clicarBotao3() {
+
+    this.numero = 3;
+
+  }
+  clicarBotao6() {
+
+    this.numero = 6;
+
+  }
+
+  clicarBotao9() {
+
+    this.numero = 9;
+
+  }
+  clicarBotao12() {
+
+    this.numero = 12;
+
+  }
+
+  somarBotao1() {
+
+    this.pont1 += this.numero;
+    this.numero = 1;
+    if (this.pont1 >=12 ) {
+
+      this.vitoria1 += 1;
+      this.pont1 = 0;
+      this.pont2 = 0;
+
+    }
+
+  }
+
+  somarBotao2() {
+
+    this.pont2 += this.numero;
+    this.numero = 1;
+    if (this.pont2 >=12 ) {
+
+      this.vitoria2 += 1;
+      this.pont1 = 0;
+      this.pont2 = 0;
+
+    }
+
+  }
+
+subtrairBotao1(){
+
+  this.numero=1
+  if(this.pont1 >=1) {
+    this.pont1 -= this.numero;
 }
+
+}
+
+
+subtrairBotao2(){
+
+  this.numero=1
+  if(this.pont2 >=1){
+
+    this.pont2 -= this.numero;
+}
+
+}
+
+
+
+
+}
+
+
